59a666bc9301c2543b0f01376ef7a7cb10846401
chuaws <chuaws@sph.com.sg>
2018-05-14 16:45:06 +0800

    updated backstop json and added puppeteer test;

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -1,6 +1,14 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   //await require('./clickAndHoverHelper')(page, scenario);
+  
+  if (scenario.label == "select_kopi-cdilogahdai") {
+    await page.waitFor('label[for=km-evaporated]');
+    await page.click('label[for=km-evaporated]');
+    await page.click('label[for=ks-stronger]');
+    await page.click('label[for=ks-sweeter]');
+    await page.waitFor(3000);
+  }
 
   // add more ready handlers here...
 };
